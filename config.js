c72f4862070d75dfcb375a081dc1fea989d461a9
esteemwilson203 <semester4fpt@gmail.com>
2024-05-29 20:58:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Thư của Lan😊😊😊",
+    introTitle: 'Gửi chị Mai yêu dấu của em (Lan)😊😊😘',
+    introDesc: `Em quý chị lắm lắm nì 🤭🤭😌`,
+    btnIntro: 'Chị quý e thì nhấn vô đây nì!!!🤭🤭',
+
+
+    title: 'Em rách túi rồi!!! Cho e em tiền đê 😂😂😂',
+    desc: '🥰🥰🥰',
+    btnYes: 'Để chuyển nè <33',
+    btnNo: 'Không chuyển đâu :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà em chỉ quý mỗi chị<3',
+    btnReply: 'Ấn vào đây để quét mã QR nè<3',
+    reply: 'Cho hẳn 1 triệu ní <33333333',
+    mess: 'WOW 🥰. Em lại thích quá 😘😘',
+    messDesc: 'E quý chị nhiều ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'qr.html'
+}
